f9ac616e608e26bfc3e4435285589a6c8fb926f4
dbsrn331 <yungu@userinsight.co.kr>
2026-01-07 18:01:06 +0900

    채팅 UI 추가

---
+++ src/main/java/kr/co/userinsight/study/controller/ChatController.java
@@ -0,0 +1,15 @@
+package kr.co.userinsight.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+
+  @GetMapping("")
+  public String chat() {
+    return "chat";
+  }
+}
