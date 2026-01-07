19b54c5ee48d99dff1280a257f06e2a894a93d89
dbsrn331 <yungu@userinsight.co.kr>
2026-01-07 19:50:57 +0900

    채팅 UI 추가

--- src/main/java/kr/co/userinsight/study/controller/ChatController.java
+++ src/main/java/kr/co/userinsight/study/controller/ChatController.java
@@ -1,15 +1,30 @@
 package kr.co.userinsight.study.controller;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
-  @GetMapping("")
-  public String chat() {
-    return "chat";
+  @GetMapping("/enterence")
+  public String enterence() {
+    return "chat/enterence";
+  }
+
+  @PostMapping("/room")
+  public String room(Model model, @RequestParam("username") String username) {
+    // 유효성 검사
+    if (StringUtils.isBlank(username)) {
+      return "redirect:/chat/enterence";
+    }
+
+    model.addAttribute("username", username);
+    return "chat/room";
   }
 }
