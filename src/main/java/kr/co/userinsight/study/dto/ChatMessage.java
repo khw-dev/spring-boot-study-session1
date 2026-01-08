a26cb367c85e957d704c7bafeb650565c7dc68a7
dbsrn331 <yungu@userinsight.co.kr>
2026-01-08 11:06:04 +0900

    채팅 API 코드 구현

---
+++ src/main/java/kr/co/userinsight/study/dto/ChatMessage.java
@@ -0,0 +1,17 @@
+package kr.co.userinsight.study.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ChatMessage {
+
+  private String username;
+
+  private String message;
+}
